9e3e22802f75a9617a6e33d0a979d76cec0bff0e
9dbit <154832153+9dbit@users.noreply.github.com>
2026-09-23 10:42:13 +0700

    Export iOS interop self-test helper

--- mobile/ios-preview/modules/telemetry-ios-native/index.ts
+++ mobile/ios-preview/modules/telemetry-ios-native/index.ts
@@ -1,2 +1,4 @@
 export { default } from './src/TelemetryIosNativeModule';
+export { default as TelemetryInteropSelfTest } from './src/TelemetryInteropSelfTestModule';
+export type { TelemetryInteropSelfTestResult } from './src/TelemetryInteropSelfTestModule';
 export * from './src/TelemetryIosNative.types';
